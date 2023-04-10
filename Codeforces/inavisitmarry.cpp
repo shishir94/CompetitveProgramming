06ad378d063658d733f34c9aaefc08f9050e140a
shishir94 <shishirkushwah@gmail.com>
2023-04-10 18:52:08 +0530

    add

---
+++ Codeforces/inavisitmarry.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+ios_base::sync_with_stdio(false);
+cin.tie(NULL);
+int t;
+cin>>t;
+while(t--){
+    int a,b;
+    cin>>a>>b;
+    cout<<"2"<<endl;
+    cout<<a-1<<" "<<1<<endl;
+    cout<<a<<" "<<b<<endl;
+}
+return 0;
+}
