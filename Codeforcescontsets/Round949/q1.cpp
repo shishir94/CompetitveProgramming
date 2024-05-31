a14a7b5089b44dad4cc345a76c199394279f89a7
Shishir Kushwah <shishirkushwah@gmail.com>
2024-05-31 17:24:59 +0530

    add

---
+++ Codeforcescontsets/Round949/q1.cpp
@@ -0,0 +1,33 @@
+#include<iostream>
+#include<bits/stdc++.h>
+using namespace std;
+
+
+long long modular_power(long long x , long long y , long long p)
+{
+ 
+long long res = 1;
+while(y>0){
+if(y%2==1){
+res = (res*x)%p;
+}
+y = y>>1;
+x = (x*x)%p;
+}
+return res%p;
+}
+
+
+int main(){
+ios_base::sync_with_stdio(false);
+cin.tie(NULL);
+int t;
+cin>>t;
+while(t--){
+    long long l,r;
+    cin>>l>>r;
+
+    cout<<(int)log2(r)<<endl;
+}
+return 0;
+}
